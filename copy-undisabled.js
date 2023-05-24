4983bd5de899c69a7ba7ee8331b06add575ac6f6
Singu <singu@singu.top>
2023-05-24 16:48:38 +0800

    Update copy-undisabled.js

--- copy-undisabled.js
+++ copy-undisabled.js
@@ -4,7 +4,8 @@
 // @version      0.1
 // @description  现在你可以自由的复制代码块的内容了
 // @author       Singu
-// @match        https://*
+// @match        http://*/*
+// @match        https://*/*
 // @icon         https://avatars.githubusercontent.com/u/23053545?v=4
 // @grant        none
 // ==/UserScript==
@@ -14,15 +15,21 @@
     let counter = 0;
     let max_exec_times = 30;
     let intervalId = undefined;
+    // 阻止任何 Copy 事件监听器的执行
+    document.addEventListener('copy', e => e.stopImmediatePropagation(), true);
+    // 定期对页面元素进行检查
     intervalId = setInterval(function () {
-        let els = document.querySelectorAll('*');
-        for (let el of els) {
+        // 更新计数器
+        counter++;
+        // 当重复执行到第 N 次时，停止定时器
+        if (counter >= max_exec_times) {
+            clearInterval(intervalId);
+        }
+        // 遍历所有页面元素
+        document.querySelectorAll('*').forEach(el => {
+            // 解除对 copy 功能的限制
             el.style.userSelect = 'unset';
             el.style.webkitUserSelect = 'unset';
-        }
-        counter++;
-        if (counter >= max_exec_times) {
-           clearInterval(intervalId);
-       }
+        });
     }, 500);
 })();
